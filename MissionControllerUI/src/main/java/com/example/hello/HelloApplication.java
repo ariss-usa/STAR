2f15c4d71ce760e85567a064673d1e3b97bc0ca6
unshrawal <32817861+unshrawal@users.noreply.github.com>
2025-01-10 12:38:29 -0500

    Fix centering & window size issue on new image

--- MissionControllerUI/src/main/java/com/example/hello/HelloApplication.java
+++ MissionControllerUI/src/main/java/com/example/hello/HelloApplication.java
@@ -1,11 +1,13 @@
 package com.example.hello;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -23,14 +25,20 @@ public class HelloApplication extends Application {
         System.out.println(HelloApplication.class.getResource("newGUI.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("newGUI.fxml"));
         Parent root = fxmlLoader.load();
-
         Scene scene = new Scene(root, 665, 466);
-        
+
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setTitle("Mission Controller");
         stage.setResizable(false);
         stage.setScene(scene);
+
+        Platform.runLater(()-> {
+            stage.setWidth(665);
+            stage.setHeight(466);
+            stage.centerOnScreen();
+        });
         stage.show();
+        
         stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             public void handle(WindowEvent we) {                
                 try(ZContext ctx = new ZContext()){
